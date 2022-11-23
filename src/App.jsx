3b1dffaa7b4bb561426b97b6ac0512ed46646571
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-23 08:15:53 +0100

    manipulate the state and add function that handle Change in value

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,28 @@ function App() {
    
   const [cantidad, setCantidad] = useState(10000);
 
+  function handleChange(e){
+    setCantidad(+e.target.value);
+  }
+
   
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header /> 
 
-      <input type="range" className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600" />
+      <input type="range" className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      onChange={ handleChange }
+      />
+
+
+
+      <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>
+        {cantidad}
+      </p>
     </div>
   )
 }
+
 
 export default App
