90a0b0ce409088923e8c2f4c8e92214bc1eeb7c5
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-23 08:47:52 +0100

    add functions add allow move to move the loan value with and add up and down value

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,49 @@ function App() {
     setCantidad(+e.target.value);
   }
 
+  function handleClickDecremento(){
+    const valor = cantidad - STEP;
+
+    if (valor < MIN) {
+      alert('Cantidad no valida');
+      return;
+    }
+
+    setCantidad(valor);
+  }
   
+  function handleClickIncremento(){
+    const valor = cantidad + STEP;
+
+    if (valor > MAX) {
+      alert('Cantidad no valida');
+      return;
+    }
+
+    setCantidad(valor);
+  }
+
+
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header /> 
+
+      <div className='flex justify-between my-6'>
+        <button
+        type='button'
+        className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full 
+        hover:outline-none hover:ring-2 hover:ring-offset-2
+        hover:ring-lime-500'
+        onClick={handleClickDecremento}>-</button>
+
+       <button
+        type='button'
+        className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full 
+        hover:outline-none hover:ring-2 hover:ring-offset-2
+        hover:ring-lime-500'
+        onClick={handleClickIncremento}>+</button>
+        </div>
 
       <input type="range" className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
       onChange={ handleChange }
